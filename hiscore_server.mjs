a9f33c6eab8c247bb4be29669f6fb1a55e2b3d74
Matthew Evans <mevans+git@wtfsystems.net>
2022-11-21 19:50:20 -0500

    sync

--- hiscore_server.mjs
+++ hiscore_server.mjs
@@ -29,14 +29,25 @@ console.log(`Press Ctrl+C to exit`)
 const server = https.createServer(settings.serverOpts, (req, res) => {
     req.on('error', (error) => { console.error(error) })
 
-    var cmdRoute = req.url.substring(1, req.url.indexOf('?'))
-
-    var args = req.url.slice(req.url.indexOf('?') + 1, req.url.length).split('&')
-    console.log(cmdRoute)
-    console.log(args)
+    //  Which command to run
+    const cmdRoute = req.url.substring(1, req.url.indexOf('?'))
+    //  Parameters to the command
+    const cmdArgs = req.url.slice(req.url.indexOf('?') + 1, req.url.length).split('&')
 
     res.writeHead(200)
-    res.end(`Connected`)
+    if(cmdRoute === 'getkey' && req.method == `GET`) {
+        res.end(`Here is the key!`)
+    } else if(cmdRoute === 'postdata') {
+        res.end(`Give data!`)
+    } else {
+        res.statusCode = 404
+        res.end(`404`)
+    }
+
+    //console.log(req.method)
+    console.log(req.url)
+    console.log(cmdRoute)
+    console.log(cmdArgs)
 })
 
 server.listen(settings.port, () => { console.log(`Running server on port ${settings.port}`) })
